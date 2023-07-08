d9d6cfb5cf7690a29a26f2040d4c82f5fee4d51e
popina1994 <zd130033d@student.etf.bg.ac.rs>
2023-07-08 11:08:49 +0100

    1. Stress Testing.

--- Test/test.cpp
+++ Test/test.cpp
@@ -2,32 +2,8 @@
 #include "../GraphStorage.h"
 #include <random>
 
-TEST(TestingBasicFunctionality, VertexCreation) {
-    using namespace MemGraph;
-    GraphStorage gs;
-    std::random_device rd;  // a seed source for the random number engine
-    std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(0, 99'999);
 
-    try {
-
-        for (uint32_t idx = 0; idx < 100'000; idx++)
-        {
-            Vertex::VERTEX_ID vId = gs.CreateVertex();
-            gs.AddLabel(vId, Label("A"));
-        }
-    }
-    catch (std::exception& e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    for (uint32_t idx = 0; idx < 100'000; idx++)
-    {
-        gs.CreateEdge(idx, distrib(gen));
-    }
-}
-
-TEST(TestingBasicFunctionality, ShortestPath) {
+TEST(TestingBasicFunctionality, BasicFunctionality) {
     using namespace MemGraph;
     GraphStorage gs;
     EXPECT_EQ(gs.CreateVertex(), 0);
@@ -58,4 +34,37 @@ TEST(TestingBasicFunctionality, ShortestPath) {
     EXPECT_EQ(v[1], 1);
     EXPECT_EQ(v[2], 2);
     EXPECT_EQ(v[3], 3);
+}
+
+TEST(TestingBasicFunctionality, StressTest) {
+    using namespace MemGraph;
+    GraphStorage gs;
+    std::random_device rd;  // a seed source for the random number engine
+    std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> distrib(0, 99'999);
+    std::uniform_int_distribution<> dist2(0, 1);
+
+    try {
+
+        for (uint32_t idx = 0; idx < 100'000; idx++)
+        {
+            Vertex::VERTEX_ID vId = gs.CreateVertex();
+            gs.AddLabel(vId, Label("A"));
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    for (uint32_t idx = 0; idx < 500'000; idx++)
+    {
+        gs.CreateEdge(distrib(gen), distrib(gen));
+    }
+
+    for (uint32_t idx = 0; idx < 100; idx++)
+    {
+        Vertex::VERTEX_ID vertStart = distrib(gen);
+        Vertex::VERTEX_ID vertEnd = distrib(gen);
+        auto v = gs.ShortestPath(vertStart, vertEnd, { "A" });
+    }
 }
